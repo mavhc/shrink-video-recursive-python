43720cc06239b4fa446ae5e4a0594356814a681f
mavhc <mark.scholes@gmail.com>
2016-04-29 13:54:17 +0100

    Update shrink-recursive.py
    esc_for_cmd_exe function was missing in previous version

--- shrink-recursive.py
+++ shrink-recursive.py
@@ -26,6 +26,33 @@ def escape_argument(arg):
 
     return escape_for_cmd_exe(arg)
 
+
+def escape_for_cmd_exe(arg):
+    # Escape an argument string to be suitable to be passed to
+    # cmd.exe on Windows
+    #
+    # This method takes an argument that is expected to already be properly
+    # escaped for the receiving program to be properly parsed. This argument
+    # will be further escaped to pass the interpolation performed by cmd.exe
+    # unchanged.
+    #
+    # Any meta-characters will be escaped, removing the ability to e.g. use
+    # redirects or variables.
+    #
+    # @param arg [String] a single command line argument to escape for cmd.exe
+    # @return [String] an escaped string suitable to be passed as a program
+    #   argument to cmd.exe
+
+    meta_chars = '()%!^"<>&|'
+    meta_re = re.compile('(' + '|'.join(re.escape(char) for char in list(meta_chars)) + ')')
+    meta_map = { char: "^%s" % char for char in meta_chars }
+
+    def escape_meta_chars(m):
+        char = m.group(1)
+        return meta_map[char]
+
+    return meta_re.sub(escape_meta_chars, arg)
+
 def isRecompressed(inputPath):
     # runs ffprobe to read the album tag, returns True if tag exists and equals "recompressed"
     cmd = os.path.join(__location__,"ffprobe") + ' -hide_banner -of default=noprint_wrappers=1  -show_entries format_tags=album -v quiet '+str(inputPath)
